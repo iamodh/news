9c907f21f8a66ada7f044d4c45eae7b8f3b6796e
Donghwan Oh <imodh97@gmail.com>
2024-10-06 18:11:15 +0900

    아티클 컨텐트 스타일링, js ajax 연결

--- main.js
+++ main.js
@@ -7,12 +7,15 @@ const URL =
 const searchBtnNode = document.querySelector(".btn-search");
 const resultNode = document.getElementById("result");
 
-searchBtnNode.addEventListener("click", () => {
+searchBtnNode.addEventListener("click", (e) => {
+  const inputNode = document.getElementById("input");
+  const keyword = inputNode.value;
+
   // ajax 통신
   const xhr = new XMLHttpRequest();
   xhr.open(
     "get",
-    "https://newsapi.org/v2/everything?q=Apple&sortBy=relavance&apiKey=28160b0d21ec4fe2b2356ab86fb65fe6",
+    `${BASIC}?q=${keyword}&sortBy=${SROTBY}&searchIn=title&apiKey=${API_KEY}`,
     true
   );
 
@@ -27,8 +30,27 @@ searchBtnNode.addEventListener("click", () => {
     console.log(articles);
     resultNode.innerHTML = "";
     articles.forEach((el) => {
-      if (el.title !== "[Removed]")
-        resultNode.innerHTML += `<p>${el.title}</p>`;
+      if (el.title !== "[Removed]" && el.author !== null) {
+        const IndexOfKeyword = el.title
+          .toLowerCase()
+          .indexOf(keyword.toLowerCase());
+
+        // 키워드 bold 처리
+        resultNode.innerHTML += `<li><a href=${
+          el.url
+        }><article class="article"><h2 class="article-title">${el.title.slice(
+          0,
+          IndexOfKeyword
+        )}<strong>${el.title.slice(
+          IndexOfKeyword,
+          IndexOfKeyword + keyword.length
+        )}</strong>${el.title.slice(IndexOfKeyword + keyword.length)}</h2>
+        <p class="article-meta"><span class="author">${
+          el.author
+        }</span><span class="createdAt">${el.publishedAt}<span></p>
+        </article>
+        <figure class="cover"><img src=${el.urlToImage} /></figure></a></li>`;
+      }
     });
   };
   xhr.send();
