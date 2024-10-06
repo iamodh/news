21a576daaa90a126919a4d9a2dea1349f0f15c1d
Donghwan Oh <imodh97@gmail.com>
2024-10-06 16:14:48 +0900

    openapi 테스트

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
 const API_KEY = "28160b0d21ec4fe2b2356ab86fb65fe6";
-const DEFAULT_URL = "https://newsapi.org/v2/";
+const BASIC = "https://newsapi.org/v2/everything";
+const SROTBY = "relavance";
+const URL =
+  "https://newsapi.org/v2/everything?q=Apple&sortBy=relavance&apiKey=28160b0d21ec4fe2b2356ab86fb65fe6";
+
+const searchBtnNode = document.querySelector(".btn-search");
+const resultNode = document.getElementById("result");
+
+searchBtnNode.addEventListener("click", () => {
+  // ajax 통신
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "get",
+    "https://newsapi.org/v2/everything?q=Apple&sortBy=relavance&apiKey=28160b0d21ec4fe2b2356ab86fb65fe6",
+    true
+  );
+
+  xhr.onprogress = function () {
+    resultNode.textContent = "검색중입니다...";
+  };
+
+  xhr.onload = function () {
+    const result = xhr.responseText;
+    const resultObj = JSON.parse(result);
+    const articles = resultObj.articles;
+    console.log(articles);
+    resultNode.innerHTML = "";
+    articles.forEach((el) => {
+      if (el.title !== "[Removed]")
+        resultNode.innerHTML += `<p>${el.title}</p>`;
+    });
+  };
+  xhr.send();
+});
